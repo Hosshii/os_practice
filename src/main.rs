d49cf1b2a1ce0f5bb65fe22ad91ee06cb6cf66b9
Hosshii <sao_heath6147.wistre@icloud.com>
2020-09-19 02:44:13 +0900

    section 2 -- A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-#![no_std]
-#![no_main]
+#![no_std] // dont link rust standard library
+#![no_main] // disable rust-level entry point
 
+extern crate rlibc;
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 /// This function is called on panic.
 #[panic_handler]
@@ -11,5 +14,12 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
+        }
+    }
     loop {}
 }
